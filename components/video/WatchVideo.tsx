983e83910df127e95c4258c7256d50601eba08c1
Peter Jeng <peterjeng1@gmail.com>
2023-06-23 13:06:14 -0400

    added title and hide mouse/nav when watching video

--- components/video/WatchVideo.tsx
+++ components/video/WatchVideo.tsx
@@ -2,6 +2,7 @@
 
 import { Movie } from "@prisma/client";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 import { BsArrowLeft } from "react-icons/bs";
 
 interface WatchVideoProps {
@@ -13,9 +14,55 @@ const WatchVideo: React.FC<WatchVideoProps> = ({
 }) => {
     const router = useRouter();
 
+    useEffect(() => {
+        const handleMouseMove = () => {
+            const videoPlayer = document.getElementById("videoPlayer");
+            const nav = document.getElementById("nav");
+
+            if (videoPlayer && nav) {
+                const videoTimer = videoPlayer.getAttribute("timer");
+                const navTimer = nav.getAttribute("timer");
+
+                if (videoTimer) {
+                    clearTimeout(videoTimer);
+                    videoPlayer.setAttribute("timer", "");
+                }
+
+                if (navTimer) {
+                    clearTimeout(navTimer);
+                    nav.setAttribute("timer", "");
+                }
+
+                const videoTime = setTimeout(() => {
+                    videoPlayer.setAttribute("timer", "");
+                    videoPlayer.classList.add("cursor-none");
+                }, 3500);
+
+                const navTime = setTimeout(() => {
+                    nav.setAttribute("timer", "");
+                    nav.classList.add("hidden");
+                }, 2500);
+
+                // @ts-ignore
+                videoPlayer.setAttribute("timer", videoTime);
+                // @ts-ignore
+                nav.setAttribute("timer", navTime);
+
+                videoPlayer.classList.remove("cursor-none");
+                nav.classList.remove("hidden");
+            }
+        }
+
+        window.addEventListener("mousemove", handleMouseMove);
+
+        return () => window.removeEventListener("mousemove", handleMouseMove);
+    }, []);
+    
+
     return ( 
         <div className="h-screen w-screen bg-black">
             <nav
+                id="nav"
                 className="
                     fixed
                     w-full
@@ -27,6 +74,7 @@ const WatchVideo: React.FC<WatchVideoProps> = ({
                     gap-8
                     bg-black
                     bg-opacity-70
+                    justify-between
                 "
             >
                 <button
@@ -38,12 +86,13 @@ const WatchVideo: React.FC<WatchVideoProps> = ({
                 >
                     <BsArrowLeft size={40} />
                 </button>
-                
-                <p className="text-white text-1xl md:text-3xl font-bold justify-center">
+                <p className="text-white text-1xl md:text-3xl font-bold">
                     {movie?.title}
                 </p>
+                <div />
             </nav>
             <video
+                id="videoPlayer"
                 autoPlay
                 controls
                 className="h-full w-full"
