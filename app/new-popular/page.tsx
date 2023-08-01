0eb9245076bfd082748c3e3ffb1a54355b10ec4f
Peter Jeng <peterjeng1@gmail.com>
2023-08-01 10:29:56 -0400

    pr test

--- app/new-popular/page.tsx
+++ app/new-popular/page.tsx
@@ -3,10 +3,16 @@ import Navbar from "@/components/navbar/Navbar";
 import { NEW_AND_POPULAR } from "@/types";
 
 const NewPopular = () => {
+    const test = "Testing for a PR"
+    const test = "Testing for a PR"
+    const test = "Testing for a PR"
+    const test = "Testing for a PR"
+
     return ( 
         <div>
             <Navbar currentPage={NEW_AND_POPULAR} />
             <NoFeature />
+            Hello! This is a just a quick test
         </div>
      );
 }
